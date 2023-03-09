48c91542869c978e123e40d64696589f2a971f51
LionMara <lionofmara@gmail.com>
2023-03-09 12:35:56 +0300

    It's been a minute

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree.
+ * binary_tree_height_perf - Measures the height of a binary tree.
  *
  * @tree: Pointer to the root node of the binary tree.
  *
